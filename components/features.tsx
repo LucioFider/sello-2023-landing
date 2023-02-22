6ff7d19bbacfb5c69e38f21bca32d5815fa0db54
LucioFider <fidelislucio404@gmail.com>
2023-02-23 09:22:25 +1000

    13th-commit

--- components/features.tsx
+++ components/features.tsx
@@ -4,11 +4,15 @@ import React from "react";
 
 type FeaturesProps = {
   children: React.ReactNode;
+  color: string;
 };
 
-export const Features = ({ children }: FeaturesProps) => {
+export const Features = ({ children, color }: FeaturesProps) => {
   return (
-    <section className="flex flex-col items-center py-[12.8rem]">
+    <section
+      className="flex flex-col items-center py-[12.8rem]"
+      style={{ "--feature-color": color } as React.CSSProperties}
+    >
       {children}
     </section>
   );
@@ -36,7 +40,7 @@ const MainFeature = ({ image, text }: MainFeatureProps) => {
       <div className="relative z-10 rounded-[14px] backdrop-blur-[6px] before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(rgba(255,_255,_255,_0.3),_rgba(255,_255,_255,_0)_120%)] before:p-[1px] before:[mask:linear-gradient(black,_black)_content-box_content-box,_linear-gradient(black,_black)] before:[mask-composite:xor] after:pointer-events-none after:absolute after:inset-0 after:rounded-[inherit] after:bg-[rgba(255,_255,_255,_0.15)] after:[mask:linear-gradient(black,transparent)]">
         <img src={image} alt="image" className="h-auto w-full" />
       </div>
-      <p className="leading-tight text-white mx-auto w-[80%] text-4xl my-16">
+      <p className="leading-tight text-white mx-auto md:w-[80%] text-2xl  md:text-4xl my-16">
         {text}
       </p>
       <hr className="mb-[7.2rem] h-[1px] border-none bg-[linear-gradient(to_right,transparent,rgba(255,255,255,0.1)_50%,transparent)]" />
@@ -54,14 +58,14 @@ type FeatureGridProps = {
 
 const FeatureGrid = ({ features }: FeatureGridProps) => {
   return (
-    <div className="mb-[14rem] w-full gap-y-9 text-md text-primary-text grid grid-cols-3 text-md">
+    <div className="mb-[14rem] w-full gap-y-9 text-md text-primary-text grid grid-cols-2 md:grid-cols-3 text-sm md:text-md">
       {features.map(({ title, text, icon: Icon }) => (
         <div
-          className="w-[25.6rem] [&_svg]:inline [&_svg]:fill-white [&_svg]:mr-[6px] [&_svg]:mb-[2px]"
+          className="max-w-[25.6rem] [&_svg]:inline [&_svg]:fill-white [&_svg]:mr-[6px] [&_svg]:mb-[2px]"
           key={title}
         >
           <Icon />
-          <span className="text-white"> {title}</span>
+          <span className="block text-white md:inline"> {title}</span>
           {text}
         </div>
       ))}
@@ -80,18 +84,18 @@ type FeatureCardsProps = {
 
 const FeatureCards = ({ features }: FeatureCardsProps) => {
   return (
-    <div className="grid w-full gap-6 grid-cols-2 ">
+    <div className="grid w-full gap-6 grid-cols-1 md:grid-cols-2 ">
       {features.map(({ title, text, image, imageClassName }) => (
         <div
           key={title}
-          className="relative overflow-hidden bg-glass-gradient border border-transparent-white p-14 rounded-[4.8rem]"
+          className="relative overflow-hidden aspect-[1.1/1] bg-glass-gradient border border-transparent-white p-14 rounded-[2.4rem] md:rounded-[4.8rem]"
         >
           <h3 className="mb-2 text-2xl text-white">{title}</h3>
           <p className="text-md text-primary-text max-w-[31rem]">{text}</p>
           <img
-            className={classNames("absolute", imageClassName)}
+            className={classNames("absolute max-w-none", imageClassName)}
             src={image}
-            alt=""
+            alt="product"
           />
         </div>
       ))}
